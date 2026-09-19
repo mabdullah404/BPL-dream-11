ddbe33f38bac9bbdd7da66238b08f98c14f79a3f
mabdullah404 <mabdullah539629@gmail.com>
2026-09-19 15:23:39 +0600

    Selectedplayers component added

--- src/components/Players/Players.tsx
+++ src/components/Players/Players.tsx
@@ -1,7 +1,8 @@
-import { use } from "react";
+import { use, useState } from "react";
 
 import type { PlayersType } from "../../types/PlayersType";
 import AvailablePlayers from "./AvailablePlayers";
+import Selectedplayers from "./Selectedplayers";
 
 
 interface PlayersProps {
@@ -14,26 +15,33 @@ const Players = ({ playersPromise }: PlayersProps) => {
   // Promise resolve করে actual player data পাওয়া
   const players = use(playersPromise);
 
-  console.log(players);
+  const [buttonType , setButtontype] =useState("available")
+
+  // console.log(players);
+
+  const handleUpdateBtnType = (type : "available" | "selected") =>{
+    setButtontype(type) ;
+
+  }
 
   return (
     <div className="max-w-7xl mx-auto sm:px-6 lg:px-8"> 
 
        <div className="flex justify-between my-6">
-        <h1 className="font-bold text-2xl">Available Players</h1>
+        <h1 className="font-bold text-2xl">{buttonType=== "available" ? "Available Players" : "Selected Players"}</h1>
 
 
         <div className="flex justify-between ">
 
-        <button className="btn btn-success">Available</button>
-        <button className="btn">Selected</button>
+        <button onClick={()=> handleUpdateBtnType("available")} className={`btn  ${buttonType === "available" ? "btn-success" : ""} rounded-r-none`}>Available</button>
+        <button onClick={()=> handleUpdateBtnType("selected")} className={`btn ${buttonType === "selected" ? "btn-success" : ""} rounded-l-none`}>Selected</button>
 
         </div>       
        </div>
 
 
 
-      <AvailablePlayers players= {players}></AvailablePlayers>
+      {buttonType === "available" ? (<AvailablePlayers players= {players}></AvailablePlayers>) : (<Selectedplayers  />)}
     </div>
   );
 };
---
+++ src/components/Players/Selectedplayers.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Selectedplayers = () => {
+    return (
+        <div>
+            Selected players : 
+        </div>
+    );
+};
+
+export default Selectedplayers;
